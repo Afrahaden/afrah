a171cf4a3f53460ddfa73e2bbedc79401989d765
Afrah Aden <afrahaden05@gmail.com>
2022-09-17 11:36:33 +0300

    Updating my title

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Designer", "Web Develper", "Mobile App Developer"],
+        strings: ["Software engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Designer", "Web Develper", "Mobile App Developer"],
+        strings: ["Software engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
