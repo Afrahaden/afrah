3d4848b78d03c8b186312eb06171b1f22041ffd4
Afrah Aden <afrahaden05@gmail.com>
2023-03-05 03:39:36 +0300

    Update

--- script.js
+++ script.js
@@ -1,83 +1,58 @@
-// Get the navbar element
-const navbar = document.querySelector('.navbar');
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
 
-// Get the scroll-up button element
-const scrollUpBtn = document.querySelector('.scroll-up-btn');
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-// Get the menu button element
-const menuBtn = document.querySelector('.menu-btn');
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
 
-// Get all menu items
-const menuItems = document.querySelectorAll('.navbar .menu li a');
+    $('.navbar .menu li a').click(function () {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
-// Add event listener for window scroll
-window.addEventListener('scroll', function () {
-// sticky navbar on scroll script
-if (window.scrollY > 20) {
-navbar.classList.add('sticky');
-} else {
-navbar.classList.remove('sticky');
-}
+    // toggle menu/navbar script
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 
-// scroll-up button show/hide script
-if (window.scrollY > 500) {
-scrollUpBtn.classList.add('show');
-} else {
-scrollUpBtn.classList.remove('show');
-}
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
-
-// Add event listener for scroll-up button click
-scrollUpBtn.addEventListener('click', function () {
-// Smoothly scroll to top of the page
-window.scrollTo({
-top: 0,
-behavior: 'smooth'
-});
-});
-
-// Add event listener for menu items click
-menuItems.forEach(function (item) {
-item.addEventListener('click', function () {
-// Smoothly scroll to the corresponding section
-document.querySelector(this.getAttribute('href')).scrollIntoView({
-behavior: 'smooth'
-});
-});
-});
-
-// Add event listener for menu button click
-menuBtn.addEventListener('click', function () {
-// Toggle active class on menu and menu button
-navbar.querySelector('.menu').classList.toggle('active');
-menuBtn.querySelector('i').classList.toggle('active');
-});
-
-// Owl carousel script
-const carousel = document.querySelector('.carousel');
-if (carousel) {
-const carouselOptions = {
-margin: 20,
-loop: true,
-autoplay: true,
-autoplayTimeOut: 2000,
-autoplayHoverPause: true,
-responsive: {
-0: {
-items: 1,
-nav: false
-},
-600: {
-items: 2,
-nav: false
-},
-1000: {
-items: 3,
-nav: false
-}
-}
-};
-
-// Initialize owl carousel
-const owl = new OwlCarousel(carousel, carouselOptions);
-}
